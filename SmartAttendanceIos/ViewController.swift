9606f4f2cd437b234ec2a4d7bb0f1fde2ca3ffa3
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-05-08 14:15:01 -0400

    Initial Commit

---
+++ SmartAttendanceIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartAttendanceIos
+//
+//  Created by Yug Rawal on 08/05/20.
+//  Copyright © 2020 Yug Rawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SmartAttendanceIosTests/SmartAttendanceIosTests.swift
@@ -0,0 +1,34 @@
+//
+//  SmartAttendanceIosTests.swift
+//  SmartAttendanceIosTests
+//
+//  Created by Yug Rawal on 08/05/20.
+//  Copyright © 2020 Yug Rawal. All rights reserved.
+//
+
+import XCTest
+@testable import SmartAttendanceIos
+
+class SmartAttendanceIosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
